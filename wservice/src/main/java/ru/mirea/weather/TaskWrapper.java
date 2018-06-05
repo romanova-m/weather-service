a9ce403eb26f6252ef08aed521278c7d4a608c33
yaaskatt <yaaskatt@yandex.ru>
2018-06-05 19:06:56 +0300

    Executor test

--- wservice/src/main/java/ru/mirea/weather/TaskWrapper.java
+++ wservice/src/main/java/ru/mirea/weather/TaskWrapper.java
@@ -8,7 +8,7 @@ public class TaskWrapper {
     public ChannelHandlerContext ctx;
     public Task task;
 	
-    public TaskWrapper( Task task, ChannelHandlerContext ctx) {
+    public TaskWrapper(Task task, ChannelHandlerContext ctx) {
         this.ctx=ctx;
         this.task=task;
     }
--- wservice/src/test/java/ru/mirea/weather/TaskExecutorTest.java
+++ wservice/src/test/java/ru/mirea/weather/TaskExecutorTest.java
@@ -1,19 +1,22 @@
 package ru.mirea.weather;
 
+import io.netty.channel.ChannelHandlerContext;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
 
+
 public class TaskExecutorTest {
 
-    /*CustomQueue inQueue;
+    CustomQueue inQueue;
     CustomQueue outQueue;
     int testSize;
-    Task task;
+    TaskWrapper task;
+    ChannelHandlerContext ctx;
 
     public TaskExecutorTest() {
         testSize = 3;
-        task = new Task(1, "Moscow");
+        task = new TaskWrapper(new Task(1, "Moscow"), ctx);
     }
 
     @Before
@@ -37,6 +40,7 @@ public class TaskExecutorTest {
         } catch (InterruptedException e) {}
         excStream.interrupt();
         assertEquals(outQueue.size(), 1);
+        assertEquals(outQueue.poll().task.city, task.task.city);
     }
-    */
+    
 }
